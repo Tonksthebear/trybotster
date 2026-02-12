065cf5d936b2236281aa53bb42bcdc9230f1ef2d
Botster Maintainer <maintainer@example.invalid>
2026-02-12 11:03:54 -0800

    Update coturn-start.sh

--- config/coturn-start.sh
+++ config/coturn-start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+INTERNAL_IP=$(hostname -i | awk '{print $1}')
 cp /etc/turnserver.conf /tmp/turnserver.conf
+sed -i "s|external-ip=5.78.156.108|external-ip=5.78.156.108/$INTERNAL_IP|" /tmp/turnserver.conf
 echo "static-auth-secret=$TURN_SECRET" >> /tmp/turnserver.conf
 exec turnserver -c /tmp/turnserver.conf
